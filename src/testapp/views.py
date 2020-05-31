dc4621813bebe963bfac814eac949ecf676e6cb9
gv1410 <gv_1410@tut.by>
2020-05-31 17:39:35 +0300

    31/05/2020

--- src/testapp/views.py
+++ src/testapp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+import requests
+import datetime
 # Create your views here.
 
 def test(request):
-    return HttpResponse('<h1> !!!!Hello Django!!!!</h1>')
+    req = requests.get('https://www.nbrb.by/api/exrates/rates/298?ondate=2016-7-05')
+    result = req.json()
+    rate = result.get('Cur_OfficialRate')
+    r = random.randint(0, 100)
+    data = [1, 3, 6, 5, 8, 77, 8]
+
+    date = datetime.datetime.now()
+
+    context = {'some_value': r, 'rate': rate, 'my_data': data, 'now': date}
+    return render(request, template_name='testapp/test.html', context=context)
